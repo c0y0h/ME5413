c010dd518e47e89ca57ccb8c7027aa64db699142
wang <renjiewang31@gmail.com>
2023-02-06 16:41:04 +0800

    task 2 code correct

--- HW1/Group9_Homework1/Task2/Code/Execution_2B.m
+++ HW1/Group9_Homework1/Task2/Code/Execution_2B.m
@@ -1,30 +1,34 @@
+clc;
+clear;
+%% Preprocess
 % Create classes
 classes = [
-    "Others1"
-    "Others2"
-    "Others3"
-    "Drivable_Surface"
-    "Others4"
-    "Others5"
-    "Others6"
-    "Others7"
+    "Others"
+    "Others"
+    "Others"
+    "DrivableSurface"
+    "Others"
+    "Others"
+    "Others"
+    "Others"
     "Vehicles"
     "Pedestrian"
-    "Bicycles_Motorcycles"
+    "BicyclesMotorcycles"
     ];
+%% Segementation
 data = load('deeplabv3plusResnet18CamVid.mat'); 
 net = data.net;
 for i=1:10
     No = num2str(i);
     ownImagePath = ['own_test_images\',No,'.jpg'];
-    I = imread(ownImagePath); % 2B
-    C = semanticseg(I, net);
+    img = imread(ownImagePath); % 2B
+    c_labels = semanticseg(img, net);
     cmap = camvidColorMap;
     % Fuse the categorical labels with the original image.
-    B = labeloverlay(I,C,'Colormap',cmap,'Transparency',0.4);
+    output_image = labeloverlay(img,c_labels,'Colormap',cmap,'Transparency',0.4);
     % Change to your absolute file path if needed
     savePath_2B = ['C:\Users\LENOVO\Desktop\ME5413\HW1\Group9_Homework1\Task2\Results\result_2B\','labeled_test_image_',No,'.jpg'];
-    imwrite(B,savePath_2B);
-    imshow(B);
+    imwrite(output_image,savePath_2B);
+    imshow(output_image);
     pixelLabelColorbar(cmap, classes);
 end
